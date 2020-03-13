8390ac1e8fcd8b3fb9d67828d12f739e375eb1c8
Ariel Schiavoni <arielschiavoni@gmail.com>
2020-03-13 21:01:37 +0100

    fix polybar fonts and remove some modules

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,10 +3,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar topbar &
+echo "---" | tee -a /tmp/polybar_topbar.log
+polybar topbar >/tmp/polybar_topbar.log 2>&1 &
 
 echo "Polybar launched..."
