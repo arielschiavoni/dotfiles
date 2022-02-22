6a98c1004b87ee5dff6def68fb82927e57f25715
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-02-22 18:09:15 +0100

    fix(fish): adjust neovim require path

--- fish/.config/fish/functions/ctrldot.fish
+++ fish/.config/fish/functions/ctrldot.fish
@@ -1,3 +1,3 @@
 function ctrldot -d "Launch Neovim dotfiles finder from the shell"
-  nvim -c 'lua require(\'my.plugins.telescope\').find_dotfiles()'
+  nvim -c 'lua require(\'user.plugins.telescope\').find_dotfiles()'
 end
--- fish/.config/fish/functions/ctrlp.fish
+++ fish/.config/fish/functions/ctrlp.fish
@@ -1,3 +1,3 @@
 function ctrlp -d "Launch Neovim file finder from the shell"
-  nvim -c 'lua require(\'my.plugins.telescope\').find_files()'
+  nvim -c 'lua require(\'user.plugins.telescope\').find_files()'
 end
