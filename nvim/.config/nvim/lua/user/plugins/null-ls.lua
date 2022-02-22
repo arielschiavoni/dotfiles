5a12c9693425af5413a15ab8a19a54b027d839a4
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-02-22 22:41:26 +0100

    feat(nvim): format on save

--- nvim/.config/nvim/lua/user/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/user/plugins/null-ls.lua
@@ -11,4 +11,9 @@ null_ls.setup({
     formatting.prettier,
     diagnostics.eslint,
   },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    end
+  end,
 })
