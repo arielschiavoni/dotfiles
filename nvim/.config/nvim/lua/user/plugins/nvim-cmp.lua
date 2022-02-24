cc701d3d0108d7237a3340b63016a41bd84ce8e5
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-02-24 15:20:03 +0100

    fix(nvim): adjust completion & snippet mappings

--- nvim/.config/nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/user/plugins/nvim-cmp.lua
@@ -7,15 +7,30 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<cr>"] = cmp.mapping.confirm(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      local luasnip = require("luasnip")
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        luasnip.expand_or_jumpable()
+        luasnip.expand_or_jump()
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      local luasnip = require("luasnip")
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
