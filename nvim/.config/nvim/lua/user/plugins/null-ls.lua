4647de24e73b1235022b3c10813b6c13c98dcb23
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-02-25 20:28:01 +0100

    feat(nvim): configure prettierd & eslint_d

--- nvim/.config/nvim/lua/user/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/user/plugins/null-ls.lua
@@ -2,14 +2,19 @@ local null_ls = require("null-ls")
 local builtins = null_ls.builtins
 local formatting = builtins.formatting
 local diagnostics = builtins.diagnostics
+local code_actions = builtins.code_actions
 
 null_ls.setup({
+  debug = false,
   sources = {
     formatting.stylua.with({
       args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
     }),
-    formatting.prettier,
-    diagnostics.eslint,
+    formatting.prettierd,
+    diagnostics.eslint_d.with({
+      timeout = 20000,
+    }),
+    code_actions.eslint_d.with({ timeout = 20000 }),
   },
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
