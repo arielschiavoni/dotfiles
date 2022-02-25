cbc301e55d7494eb889d170b509515071f33fca6
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-02-25 14:54:07 +0100

    fix(nvim): sync packages when plugins.lua is changed

--- nvim/.config/nvim/lua/user/core/plugins.lua
+++ nvim/.config/nvim/lua/user/core/plugins.lua
@@ -11,6 +11,14 @@ if fn.empty(fn.glob(install_path)) > 0 then
   })
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
 require("packer").startup(function(use)
   use("lewis6991/impatient.nvim")
 
