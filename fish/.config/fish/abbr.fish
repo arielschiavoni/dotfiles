56f169ce6409b144309fbb8232f5c03714dca765
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-03-18 19:32:46 +0100

    feat(fish): customize git commit abbr

--- fish/.config/fish/abbr.fish
+++ fish/.config/fish/abbr.fish
@@ -10,4 +10,5 @@ abbr --add --global sf 'source ~/.config/fish/config.fish'
 abbr --add --global edf 'nvim ~/personal/dotfiles'
 abbr --add --global oc 'nvim ~/.config'
 abbr --add --global gclb 'git clone --bare'
+abbr --add --global gc 'git commit -v -m'
 
