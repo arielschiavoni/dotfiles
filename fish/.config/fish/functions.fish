1b9c3b8966b36234cc6637e70ae7e8c15b89c146
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-04-08 21:19:28 +0200

    feat(fish): setup cht and cleanup utils

--- fish/.config/fish/functions.fish
+++ fish/.config/fish/functions.fish
@@ -1,12 +1,3 @@
-
-function ctrldot -d "Launch Neovim dotfiles finder from the shell"
-  nvim -c 'lua require(\'user.plugins.telescope\').find_dotfiles()'
-end
-
-function ctrlp -d "Launch Neovim file finder from the shell"
-  nvim -c 'lua require(\'user.plugins.telescope\').find_files()'
-end
-
 function find_password -d "Fuzzy searches a password in all gopass stores and copies it to the clipboard after selection"
   # disable preview of password :-)
   # gopass show --clip (gopass ls --flat | fzf)
@@ -16,20 +7,17 @@ function find_password -d "Fuzzy searches a password in all gopass stores and co
 end
 
 function fish_user_key_bindings -d "Set custom key bindings"
-  fzf_key_bindings
-  bind \cp ctrlp
-  bind \cj fzf-cd-widget # j for jump
+  # https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_default_key_bindings.fish
+  fish_default_key_bindings
+  # https://fishshell.com/docs/current/interactive.html#vi-mode-commands
+  # https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_vi_key_bindings.fish
+  # fish_vi_key_bindings
+
+  bind \ck up-or-search
+  bind \cj down-or-search
+
   bind \cg find_password # TODO: find out why the prompt wait for an CR to finish the command
   bind \cf tmux-sessionizer
   bind \cs tmux-find-session
-end
-
-function worktree -d "Creates a git worktree in the current working directory and sets its origin"
-  set path $argv[1]
-  set branch $argv[1]
-  set origin origin
-
-  git worktree add $path $branch
-  git branch --set-upstream-to=$origin/$branch $branch
 end
 
