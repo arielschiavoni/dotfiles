2430bb33b2bc8fe1a6cc035004a91cd7f77c097c
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-05-25 15:43:17 +0200

    fix(nvim): fix command mode autocompletion

--- nvim/.config/nvim/lua/user/core/plugins.lua
+++ nvim/.config/nvim/lua/user/core/plugins.lua
@@ -88,7 +88,6 @@ require("packer").startup(function(use)
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
     },
     config = function()
       require("user.plugins.nvim-cmp")
@@ -97,11 +96,12 @@ require("packer").startup(function(use)
 
   use({
     "L3MON4D3/LuaSnip",
-    after = "nvim-cmp",
     config = function()
       require("user.plugins.luasnip")
     end,
   })
+
+  use({ "saadparwaiz1/cmp_luasnip" })
 
   use({
     "JoosepAlviste/nvim-ts-context-commentstring",
--- nvim/.config/nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/user/plugins/nvim-cmp.lua
@@ -1,36 +1,56 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+local function tab(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
+  else
+    -- F("<Tab>")
+    fallback()
+  end
+end
+
+local function shtab(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  elseif luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    -- F('<S-Tab>')
+    fallback()
+  end
+end
+
+local function enterit(fallback)
+  if cmp.visible() and cmp.get_selected_entry() then
+    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+  else
+    -- F("<CR>")
+    fallback()
+  end
+end
 
 cmp.setup({
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = {
-    ["<cr>"] = cmp.mapping.confirm(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      local luasnip = require("luasnip")
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        luasnip.expand_or_jumpable()
-        luasnip.expand_or_jump()
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      local luasnip = require("luasnip")
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(tab, { "i", "s", "c" }),
+    ["<S-Tab>"] = cmp.mapping(shtab, { "i", "s", "c" }),
+    ["<CR>"] = cmp.mapping(enterit, { "i", "s" }),
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
