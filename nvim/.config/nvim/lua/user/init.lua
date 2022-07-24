f47a2d60362c7a77b4601a7a4942d5c60a248663
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-07-24 12:52:11 +0200

    feat(nvim): highlight yanked text

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -1,3 +1,16 @@
 require("user.core.settings")
 require("user.core.keymaps")
 require("user.core.plugins")
+
+-- temporarly highlight yankend region.
+-- it helps to visualise what was yanked after running commands like: ya{
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("HighlightYank", {}),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 150,
+    })
+  end,
+})
