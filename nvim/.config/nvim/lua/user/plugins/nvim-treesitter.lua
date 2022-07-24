64155e5335749a4d350132ae9d0e0fe805c83d10
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-07-24 12:22:03 +0200

    fix(nvim): improve mappings for treesiter incremental selection

--- nvim/.config/nvim/lua/user/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/nvim-treesitter.lua
@@ -36,10 +36,10 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     -- mappings for incremental selection (only available on visual mode)
     keymaps = {
-      init_selection = "gnn", -- maps in normal mode to init scope/node selection
-      node_incremental = "grn", -- increments selection to upper named parent
-      scope_incremental = "grc", -- increments selection to the upper scope (as defined in locals.scm)
-      node_decremental = "grm", -- decrements selection to the previous node
+      init_selection = "<C-Up>", -- maps in normal mode to init scope/node selection
+      node_incremental = "<C-Up>", -- increments selection to upper named parent
+      scope_incremental = "<C-Up>", -- increments selection to the upper scope (as defined in locals.scm)
+      node_decremental = "<C-Down>", -- decrements selection to the previous node
     },
   },
   -- required by "Comment" and "nvim-ts-context-commentstring" plugins to properly comment jsx/tsx files
