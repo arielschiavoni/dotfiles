b2fe2bf6a133d4dce2a38b5c2839cca105082bd9
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-10-11 13:21:40 +0200

    feat(nvim): auto reload buffer when file changes outside of nvim

--- nvim/.config/nvim/lua/ariel/autocmds.lua
+++ nvim/.config/nvim/lua/ariel/autocmds.lua
@@ -45,3 +45,26 @@ create_autocmd("FileType", {
     end, { buffer = buff, desc = "rest: preview the request cURL command" })
   end,
 })
+
+-- Automatically reload the file if it is changed outside of Nvim, see https://unix.stackexchange.com/a/383044/221410.
+-- It seems that `checktime` does not work in command line. We need to check if we are in command
+-- line before executing this command, see also https://vi.stackexchange.com/a/20397/15292 .
+create_augroup("auto_read", { clear = true })
+
+create_autocmd({ "FileChangedShellPost" }, {
+  pattern = "*",
+  group = "auto_read",
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded!", vim.log.levels.WARN, { title = "nvim-config" })
+  end,
+})
+
+create_autocmd({ "FocusGained", "CursorHold" }, {
+  pattern = "*",
+  group = "auto_read",
+  callback = function()
+    if vim.fn.getcmdwintype() == "" then
+      vim.cmd("checktime")
+    end
+  end,
+})
