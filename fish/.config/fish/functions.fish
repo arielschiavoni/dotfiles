9ac06122d943765f693f714958d463a2fdd7a49f
Ariel Schiavoni <arielschiavoni@gmail.com>
2022-11-10 17:53:02 +0100

    feat(fish): add function to kill process on port

--- fish/.config/fish/functions.fish
+++ fish/.config/fish/functions.fish
@@ -30,3 +30,10 @@ end
 function chrome -d "Google Chrome alias"
     open -na "Google Chrome" $argv
 end
+
+function kill_port --argument port -d "Kill process using the given port"
+    for pid in (lsof -i TCP:$port | awk '/LISTEN/{print $2}')
+        echo -n "Found server for port $port with pid $pid: "
+        kill -9 $pid; and echo "killed."; or echo "could not kill."
+    end
+end
