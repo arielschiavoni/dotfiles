182e92a613316f60481864a1ec622c10a1b49dd8
Ariel Schiavoni <arielschiavoni@gmail.com>
2023-03-20 16:51:38 +0100

    feat: configure tokyonight theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,5 +6,6 @@ set -U fish_greeting
 source $source_dir/env.fish
 source $source_dir/abbr.fish
 source $source_dir/functions.fish
+source $source_dir/themes/tokyonight_moon.fish
 
 fish_user_key_bindings
