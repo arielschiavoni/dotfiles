9a7d13e700b71d8ef787677d3a45a4c25024de5c
Ariel Schiavoni <arielschiavoni@gmail.com>
2023-03-20 19:36:06 +0100

    feat: setup peek markdown previewer

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,22 +1,19 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  build = "cd app && npm install",
-  ft = { "markdown" },
-  config = function()
-    vim.api.nvim_exec(
-      [[
-          function OpenMarkdownPreview (url)
-            execute "silent ! chrome --args --new-window " . a:url
-          endfunction
-      ]],
-      false
-    )
-    vim.g.mkdp_filetypes = { "markdown" }
-    vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
-    vim.g.mkdp_auto_close = 1
-
-    -- keymaps
-    vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", { desc = "start markdown preview" })
-    vim.keymap.set("n", "<leader>mc", ":MarkdownPreviewStop<CR>", { desc = "stop markdown preview" })
-  end,
+  "toppair/peek.nvim",
+  build = "deno task --quiet build:fast",
+  keys = {
+    {
+      "<leader>mp",
+      function()
+        local peek = require("peek")
+        if peek.is_open() then
+          peek.close()
+        else
+          peek.open()
+        end
+      end,
+      desc = "Peek (Markdown Preview)",
+    },
+  },
+  opts = { theme = "light" },
 }
