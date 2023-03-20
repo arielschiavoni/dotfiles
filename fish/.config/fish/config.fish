06c378ce12406a9f073d68aac98c4151ae021bb2
Ariel Schiavoni <arielschiavoni@gmail.com>
2023-03-20 17:52:19 +0100

    fix: install theme using fisher plugin

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,10 +2,9 @@ set -l source_dir (dirname (status -f))
 # remove default fish greeting
 set -U fish_greeting
 
-
 source $source_dir/env.fish
 source $source_dir/abbr.fish
 source $source_dir/functions.fish
-source $source_dir/themes/tokyonight_moon.fish
 
 fish_user_key_bindings
+fish_config theme choose "TokyoNight Moon"
