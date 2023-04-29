d122ac8efc7e2dce8e7c0f8f5df5f585e8372b04
Ariel Schiavoni <arielschiavoni@gmail.com>
2023-04-29 11:46:40 +0200

    feat(nvim): configure sections in which key and enable spellcheck fixes

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -216,10 +216,25 @@ return {
   },
   {
     "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v" },
+        ["g"] = { name = "+goto" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader>f"] = { name = "+file/find" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>d"] = { name = "+debug" },
+      },
+    },
     config = function(_, opts)
       vim.o.timeout = true
       vim.o.timeoutlen = 300
-      require("which-key").setup(opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
     end,
   },
   {
