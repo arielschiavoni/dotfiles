88843e4bef1555a8574e2484ec7aea1f3e0d146e
Ariel Schiavoni <arielschiavoni@gmail.com>
2023-07-07 13:49:24 +0200

    feat(wezterm): replace iterm2 with wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = "tokyonight_moon"
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.window_close_confirmation = "NeverPrompt"
+config.font = wezterm.font_with_fallback({ "JetBrains Mono", { family = "Symbols Nerd Font Mono", scale = 0.9 } })
+config.keys = {
+  {
+    key = "n",
+    mods = "SHIFT|CTRL",
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
