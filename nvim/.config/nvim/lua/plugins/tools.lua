17e2bb7f65a0eef2ad44ee275be4b57a1d350c17
Ariel Schiavoni <arielschiavoni@gmail.com>
2023-09-21 09:41:27 +0200

    feat(nvim): don't notify on format error

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -88,6 +88,10 @@ return {
           lsp_fallback = true,
         },
         formatters_by_ft = formatters_by_ft,
+        -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+        log_level = vim.log.levels.ERROR,
+        -- Conform will notify you when a formatter errors
+        notify_on_error = true,
       })
     end,
   },
