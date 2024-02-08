4183a20eb24cd7037b4f386a0f091e61b517d75c
Ariel Schiavoni <arielschiavoni@gmail.com>
2024-02-08 11:28:35 +0100

    feat(nvim): add support to telescope to preview images with imgcat

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -172,6 +172,44 @@ return {
               ["<M-p>"] = actions_layout.toggle_preview,
             },
           },
+          preview = {
+            mime_hook = function(filepath, bufnr, opts)
+              local is_image = function(filepath)
+                local image_extensions = { "png", "jpg" } -- Supported image formats
+                local split_path = vim.split(filepath:lower(), ".", { plain = true })
+                local extension = split_path[#split_path]
+                return vim.tbl_contains(image_extensions, extension)
+              end
+
+              if is_image(filepath) then
+                local term = vim.api.nvim_open_term(bufnr, {})
+                local function send_output(_, data, _)
+                  for _, d in ipairs(data) do
+                    vim.api.nvim_chan_send(term, d .. "\r\n")
+                  end
+                end
+                -- chafa --animate=off --center=on --clear --size "${max_width}x${max_height}" "${img_path}"
+                vim.fn.jobstart({
+                  "catimg",
+                  filepath,
+                }, {
+                  on_stdout = send_output,
+                  on_stderr = function(data)
+                    vim.notify(data, vim.log.levels.ERROR)
+                  end,
+                  stdout_buffered = true,
+                  pty = true,
+                  on_std,
+                })
+              else
+                require("telescope.previewers.utils").set_preview_message(
+                  bufnr,
+                  opts.winid,
+                  "Binary cannot be previewed"
+                )
+              end
+            end,
+          },
         },
         extensions = {
           fzy_native = {
