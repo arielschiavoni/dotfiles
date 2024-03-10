b3e97a0f06eb645d2b886c829a19e368bf5cef96
Ariel Schiavoni <arielschiavoni@gmail.com>
2024-03-10 11:40:39 +0100

    fix(nvim): revert harpoon to v1

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -333,26 +333,42 @@ return {
   },
   {
     "ThePrimeagen/harpoon",
-    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
     config = function()
       local harpoon = require("harpoon")
+      local harpoon_mark = require("harpoon.mark")
+      local harpoon_ui = require("harpoon.ui")
 
-      ---@diagnostic disable-next-line: missing-parameter
-      harpoon.setup()
+      harpoon.setup({
+        global_settings = {
+          -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+          save_on_toggle = false,
+
+          -- saves the harpoon file upon every change. disabling is unrecommended.
+          save_on_change = true,
+
+          -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+          enter_on_sendcmd = false,
+
+          -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+          tmux_autoclose_windows = false,
+
+          -- filetypes that you want to prevent from adding to the harpoon list menu.
+          excluded_filetypes = { "harpoon" },
+
+          -- set marks specific to each git branch inside git repository
+          mark_branch = false,
+        },
+      })
 
       -- keymaps
-      vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():append()
-      end, { desc = "append file to harpoon list" })
-      vim.keymap.set("n", "<leader>z", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end, { desc = "toggle harpoon UI" })
-
+      vim.keymap.set("n", "<leader>a", harpoon_mark.add_file, { desc = "add file to harpoon list" })
+      vim.keymap.set("n", "<leader>z", harpoon_ui.toggle_quick_menu, { desc = "toggle harpoon UI" })
+      -- keymaps
       for index = 1, 5 do
         vim.keymap.set("n", string.format("<leader>%d", index), function()
-          harpoon:list():select(index)
+          harpoon_ui.nav_file(index)
         end, { desc = string.format("navigate to harpoon file %d", index) })
       end
     end,
