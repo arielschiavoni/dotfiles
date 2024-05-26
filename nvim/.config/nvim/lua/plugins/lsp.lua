28e946de2c5f269725d2a147f2b16a42d85817df
Ariel Schiavoni <arielschiavoni@gmail.com>
2024-05-26 19:54:01 +0200

    feat(nvim): add hxml lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -195,6 +195,7 @@ return {
         },
         bashls = true,
         html = true,
+        htmx = true,
         eslint = {
           settings = {
             -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
