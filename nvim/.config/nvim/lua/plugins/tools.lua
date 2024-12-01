7a53a9f85b31b652041973841aaccdc9db0c4f0e
Ariel Schiavoni <arielschiavoni@gmail.com>
2024-12-01 14:03:46 +0100

    fix(nvim): back to markdown-preview
    peek.nvim does not work well with webview
    render-markwdown is too verbose and conflicts with snippets

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -255,54 +255,29 @@ return {
     },
   },
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    ft = "markdown",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>rmt", "<cmd>RenderMarkdown toggle<CR>", ft = "markdown", desc = "Render markdown toggle" },
-    },
-  },
-  {
-    "toppair/peek.nvim",
-    build = "deno task --quiet build:fast",
+    "iamcco/markdown-preview.nvim",
+    ft = { "markdown" },
     keys = {
       {
         "<leader>mp",
-        function()
-          local peek = require("peek")
-          if peek.is_open() then
-            peek.close()
-          else
-            peek.open()
-          end
-        end,
+        ":MarkdownPreview<CR>",
         ft = "markdown",
-        desc = "Peek (Markdown Preview)",
+        desc = "Markdown Preview",
       },
     },
-    config = function()
-      require("peek").setup({
-        auto_load = true, -- whether to automatically load preview when entering another markdown buffer
-        close_on_bdelete = true, -- close preview window on buffer delete
-
-        syntax = true, -- enable syntax highlighting, affects performance
-
-        theme = "light", -- 'dark' or 'light'
-
-        update_on_change = true,
-
-        -- app = "webview", -- 'webview', 'browser', string or a table of strings
-        app = "webview",
-        -- app = { "Google Chrome", "--args", "--new-window" },
-
-        filetype = { "markdown" }, -- list of filetypes to recognize as markdown
-
-        -- relevant if update_on_change is true
-        throttle_at = 200000, -- start throttling when file exceeds this
-        -- amount of bytes in size
-        throttle_time = "auto", -- minimum amount of time in milliseconds
-        -- that has to pass before starting new render
-      })
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.cmd([[
+      function OpenMarkdownPreview (url)
+        execute "silent ! chrome --args --new-window " . a:url
+      endfunction
+       ]])
+      -- a custom Vim function name to open preview page, this function will receive URL as param
+      vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+      -- nvim will auto close current preview window when changing from Markdown buffer to another buffer
+      vim.g.mkdp_auto_close = 1
     end,
   },
   {
