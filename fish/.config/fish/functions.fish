66ff2aa4b1a0a199e53e0203c6e21805dedca0cf
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-01-30 15:06:14 +0100

    feat(fish): make AWS Switch profile faster

--- fish/.config/fish/functions.fish
+++ fish/.config/fish/functions.fish
@@ -103,5 +103,20 @@ function git_branch_cleanup -d "Cleanup local branches"
 end
 
 function aws_switch_profile -d "Switch AWS profile"
-  export AWS_PROFILE=$(aws configure list-profiles | fzf --prompt "Choose active AWS profile:")
+    # Check if the cache file exists
+    if test -f ~/.aws/profile_cache
+      # Read profiles from the cache and use fzf to select one
+      set selected_profile (cat ~/.aws/profile_cache | fzf --prompt "Choose active AWS profile:")
+
+      if test -n "$selected_profile"
+        echo "Selected AWS profile: $selected_profile"
+        export AWS_PROFILE=$selected_profile
+      else
+        echo "No profile selected."
+      end
+    else
+      echo "Profile cache file not found. Running `aws configure list-profiles > ~/.aws/profile_cache` to populate it."
+      aws configure list-profiles > ~/.aws/profile_cache
+      aws_switch_profile
+    end
 end
