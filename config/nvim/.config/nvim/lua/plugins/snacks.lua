7eaa613582858bc75db7b0e4b004d136c88c88e0
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-03-10 08:34:00 +0100

    feat(nvim): enable snacks git keymaps

--- config/nvim/.config/nvim/lua/plugins/snacks.lua
+++ config/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -111,14 +111,55 @@ return {
       desc = "Recent",
     },
     -- -- git
-    -- { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-    -- { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-    -- { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
-    -- { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-    -- { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-    -- -- overlaps with gdo
-    -- -- { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-    -- { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    {
+      "<leader>gb",
+      function()
+        Snacks.picker.git_branches()
+      end,
+      desc = "Git Branches",
+    },
+    {
+      "<leader>gl",
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = "Git Log",
+    },
+    {
+      "<leader>gL",
+      function()
+        Snacks.picker.git_log_line()
+      end,
+      desc = "Git Log Line",
+    },
+    {
+      "<leader>gs",
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = "Git Status",
+    },
+    {
+      "<leader>gS",
+      function()
+        Snacks.picker.git_stash()
+      end,
+      desc = "Git Stash",
+    },
+    {
+      "<leader>gD",
+      function()
+        Snacks.picker.git_diff()
+      end,
+      desc = "Git Diff (Hunks)",
+    },
+    {
+      "<leader>gf",
+      function()
+        Snacks.picker.git_log_file()
+      end,
+      desc = "Git Log File",
+    },
     -- search
     {
       "<leader>sb",
