be8b8bb7add881b486a95f2cb2f9958b78200f98
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-04-02 10:43:32 +0200

    fix(nvim): disable flash char mode as it does not work with macros

--- config/nvim/.config/nvim/lua/plugins/flash.lua
+++ config/nvim/.config/nvim/lua/plugins/flash.lua
@@ -2,12 +2,21 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  opts = {
+    modes = {
+      -- disable because they don't play well with macros
+      -- options used when flash is activated through
+      -- `f`, `F`, `t`, `T`, `;` and `,` motions
+      char = {
+        enabled = false,
+      },
     },
+  },
+    -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  },
 }
