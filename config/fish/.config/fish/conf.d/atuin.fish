bfd483051d835c9163fe018310053d651be8d6f5
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-10-19 15:01:14 +0200

    fix(fish): workaround to supress atuin error

--- config/fish/.config/fish/conf.d/atuin.fish
+++ config/fish/.config/fish/conf.d/atuin.fish
@@ -1,1 +1,6 @@
-atuin init fish | source
+#
+# atuin has a fish related issue. Need to temporarily work around it.
+# https://github.com/atuinsh/atuin/issues/2803, https://github.com/atuinsh/atuin/issues/2940
+# atuin init fish | source
+atuin init fish | sed "s/-k up/up/g" | source
+# atuin init fish | source
