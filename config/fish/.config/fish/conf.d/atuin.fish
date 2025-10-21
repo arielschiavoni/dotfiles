7ebbf6af8be8f55f41756072fc8b1a05080433fc
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-10-21 11:52:45 +0200

    Revert "fix(fish): workaround to supress atuin error"
    This reverts commit bfd483051d835c9163fe018310053d651be8d6f5.

--- config/fish/.config/fish/conf.d/atuin.fish
+++ config/fish/.config/fish/conf.d/atuin.fish
@@ -1,6 +1,1 @@
-#
-# atuin has a fish related issue. Need to temporarily work around it.
-# https://github.com/atuinsh/atuin/issues/2803, https://github.com/atuinsh/atuin/issues/2940
-# atuin init fish | source
-atuin init fish | sed "s/-k up/up/g" | source
-# atuin init fish | source
+atuin init fish | source
