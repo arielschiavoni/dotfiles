abc629f30eea688f821f031ecdd8b9af656cf075
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-10-21 16:21:02 +0200

    fix(lazygit): define XDG_CONFIG_HOME so lazygit can load its config

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ set -U fish_greeting
 
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
+set -gx XDG_CONFIG_HOME $HOME/.config
 
 # add new directories to PATH
 set -gx PATH $HOME/.cargo/bin $PATH
