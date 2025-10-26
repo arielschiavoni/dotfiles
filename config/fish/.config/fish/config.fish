809b9302cdb2d43b3b17cccc5fc3f734d2ef61af
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-10-26 18:24:01 +0100

    fix: setup tokyonight theme on eza and fish

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ set -U fish_greeting
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx EZA_CONFIG_DIR $HOME/.config/eza
 
 # add new directories to PATH
 set -gx PATH $HOME/.cargo/bin $PATH
