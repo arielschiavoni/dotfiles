3513bdf16b3a24385ee3eb5cf5469d01e4a7aa98
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-12-01 22:35:12 +0100

    feat: replace mise with fnm
    mise adds 50ms delay to shell startup agains 5ms from fnm. Other
    packages like golang, bun, prettier, aws-cdk can be installed with brew

--- config/fish/.config/fish/conf.d/mise.fish
+++
@@ -1,6 +0,0 @@
-# Cache mise activate to avoid spawning external process (~44ms saved)
-set -l cache_file ~/.cache/fish/mise_activate.fish
-if not test -f $cache_file
-    mise activate fish >$cache_file
-end
-source $cache_file
