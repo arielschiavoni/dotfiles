a27ad2859564c2c35b3270e6f29ff40f674302ed
Ariel Schiavoni <arielschiavoni@gmail.com>
2025-12-15 17:05:50 +0100

    feat(tmux): move fishshell binds to tmux

--- config/fish/.config/fish/bundle.d/keybindings.fish
+++ config/fish/.config/fish/bundle.d/keybindings.fish
@@ -3,8 +3,4 @@ fish_default_key_bindings
 # https://fishshell.com/docs/current/interactive.html#vi-mode-commands
 # https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_vi_key_bindings.fish
 # fish_vi_key_bindings
-bind ctrl-g find_password # TODO: find out why the prompt wait for an CR to finish the command
 bind ctrl-n 'nvim .'
-bind ctrl-v 'env | fzf'
-bind ctrl-d 'gh dash'
-bind ctrl-o opencode
