c378476211b7b22df9d9f2438045e21fc4f9aaac
Ariel Schiavoni <arielschiavoni@gmail.com>
2026-05-29 09:25:22 +0200

    fix(claude): don't fallback to amazon bedrock

--- config/fish/.config/fish/bundle.d/claude-code.fish
+++ config/fish/.config/fish/bundle.d/claude-code.fish
@@ -1,6 +1,6 @@
 # Enable Bedrock integration
 # https://code.claude.com/docs/en/amazon-bedrock
-set -gx CLAUDE_CODE_USE_BEDROCK 1
+# set -gx CLAUDE_CODE_USE_BEDROCK 1
 # Recommended output token settings for Bedrock
-set -gx CLAUDE_CODE_MAX_OUTPUT_TOKENS 4096
-set -gx MAX_THINKING_TOKENS 1024
+# set -gx CLAUDE_CODE_MAX_OUTPUT_TOKENS 4096
+# set -gx MAX_THINKING_TOKENS 1024
