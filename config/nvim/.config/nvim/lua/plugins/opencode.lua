0cd4741be24b7432938ab800e95ca1d55d79d00e
Ariel Schiavoni <arielschiavoni@gmail.com>
2026-06-01 09:52:57 +0200

    fix(nvim): prevent opencode.nvim to auto submit when pasting selection over

--- config/nvim/.config/nvim/lua/plugins/opencode.lua
+++ config/nvim/.config/nvim/lua/plugins/opencode.lua
@@ -14,9 +14,9 @@ return {
     }
 
     -- stylua: ignore start
-    vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode" })
+    vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ") end,                    { desc = "Ask opencode" })
     vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "x" }, "<leader>op", function() require("opencode").prompt("@this") end,                   { desc = "Add to opencode" })
+    vim.keymap.set({ "n", "x" }, "<leader>op", function() require("opencode").prompt("@this ") end,                  { desc = "Add to opencode" })
     vim.keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
     vim.keymap.set("n",        "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "opencode half page up" })
     vim.keymap.set("n",        "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "opencode half page down" })
