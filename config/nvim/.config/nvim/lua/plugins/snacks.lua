8bee4462119c6959c455f6a34b8a7b83d97133c4
Ariel Schiavoni <arielschiavoni@gmail.com>
2026-06-13 16:50:53 +0200

    fix(nvim): remove image preview and fix warning with deprecated vim.print function

--- config/nvim/.config/nvim/lua/plugins/snacks.lua
+++ config/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -77,11 +77,6 @@ return {
     statuscolumn = { enabled = true },
     words = { enabled = true },
     styles = {},
-    image = {
-      doc = {
-        inline = false,
-      },
-    },
   },
   keys = {
     -- Top Pickers & Explorer
@@ -541,7 +536,14 @@ return {
         _G.bt = function()
           Snacks.debug.backtrace()
         end
-        vim.print = _G.dd -- Override print to use snacks for `:=` command
+        -- Override print to use snacks for `:=` command (nvim 0.11+ uses vim._print)
+        if vim.fn.has("nvim-0.11") == 1 then
+          vim._print = function(_, ...)
+            dd(...)
+          end
+        else
+          vim.print = _G.dd
+        end
 
         -- Create some toggle mappings
         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
