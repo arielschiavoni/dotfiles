81528be48908929412d171af97f8a122d6eb6ab7
Ariel Schiavoni <arielschiavoni@gmail.com>
2026-06-15 11:14:44 +0200

    fix(fish): respect newlines when copying passwords or certificates

--- config/fish/.config/fish/functions/find_password.fish
+++ config/fish/.config/fish/functions/find_password.fish
@@ -1,14 +1,11 @@
 function find_password -d "Fuzzy searches a password in all gopass stores and copies it to the clipboard after selection"
-  set selection (gopass ls --flat | fzf --preview "gopass show {}")
+    set selection (gopass ls --flat | fzf --preview "gopass show {}")
 
-  if test $selection
-    set password (gopass show $selection)
-
-    if test (uname) = "Darwin"
-      echo $password | pbcopy
-    else if string match -q "*Linux*" (uname)
-      echo $password | wl-copy
+    if test $selection
+        if test (uname) = Darwin
+            gopass show $selection | pbcopy
+        else if string match -q "*Linux*" (uname)
+            gopass show $selection | wl-copy
+        end
     end
-  end
 end
-
