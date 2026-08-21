efb61dba8c63707da8dc875e81e1301a64035a7d
Ariel Schiavoni <arielschiavoni@gmail.com>
2026-08-21 11:39:55 +0200

    fix(fish): adjust exa abbr

--- config/fish/.config/fish/bundle.d/abbr.fish
+++ config/fish/.config/fish/bundle.d/abbr.fish
@@ -2,10 +2,10 @@ abbr --add --global v nvim
 abbr --add --global n npm
 abbr --add --global p pnpm
 abbr --add --global cat bat
-abbr --add --global ls 'eza --long --all --icons'
-abbr --add --global la 'eza --long --all --icons'
-abbr --add --global lt 'eza --tree --all --level 2 --icons'
-abbr --add --global tree 'eza --tree --all --level 2 --icons'
+abbr --add --global ls 'eza --long --all --icons auto'
+abbr --add --global la 'eza --long --all --icons auto'
+abbr --add --global lt 'eza --tree --all --level 2 --icons auto'
+abbr --add --global tree 'eza --tree --all --level 2 --icons auto'
 abbr --add --global sf 'source ~/.config/fish/config.fish'
 abbr --add --global gclb git_clone_bare
 abbr --add --global gc 'git commit -v -m'
@@ -15,7 +15,6 @@ abbr --add --global gwa 'git worktree add'
 abbr --add --global gjm "git jump merge"
 abbr --add --global gw! git_worktree_cleanup
 abbr --add --global gb! git_branch_cleanup
-abbr --add --global tokscale 'bunx tokscale@latest'
 alias docker podman
-alias npm secure_npm
-alias pnpm secure_pnpm
+# alias npm secure_npm
+# alias pnpm secure_pnpm
